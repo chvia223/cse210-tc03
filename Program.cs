e99d301842536eecccebc1566bbfb00596a6b912
chvia223 <via17001@byui.edu>
2021-09-29 12:48:08 -0600

    added DisplayGreetingViazzo function

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ namespace cse210_tc03
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our collaborative program.");
-            DisplayGreetingAvila();
+            DisplayGreetingViazzo();
         }
-        static void DisplayGreetingAvila()
+        static void DisplayGreetingViazzo()
         {
-            Console.WriteLine("Hello from Josue Avila.");
+            Console.WriteLine("Hello from Christian Viazzo.");
 
         }
     }
