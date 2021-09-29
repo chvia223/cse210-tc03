525eb923a22903e2f96b727340c9617c76dfe355
jojoavila <avilajosuealejandro@gmail.com>
2021-09-29 12:25:39 -0600

    adding my greeting function

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace cse210_tc03
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our collaborative program.");
+            DisplayGreetingAvila();
+        }
+        static void DisplayGreetingAvila()
+        {
+            Console.WriteLine("Hello from Josue Avila.");
+
         }
     }
 }
